3b32c09ee65163791723486be4c8d08e9bf280d6
admin <admin@0-00>
2018-10-26 15:08:05 +0900

    MemberVo  추가

---
+++ gittest1/src/test/MemberVo.java
@@ -0,0 +1,19 @@
+package test;
+
+public class MemberVo {
+	private String no;
+	private String age;
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+}
